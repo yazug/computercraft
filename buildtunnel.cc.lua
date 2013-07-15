16be362800f87e1c34c0a6cb9a2bc522b193d648
Jon Schlueter <jon.schlueter@gmail.com>
2013-07-14 21:34:33 -0400

    whitespace refactor in buildtunnel.cc.lua

--- buildtunnel.cc.lua
+++ buildtunnel.cc.lua
@@ -14,140 +14,140 @@ local window_slot = 3
 local junk_slot = 4
 local fuel_slot = 16
 function checkRefuel()
-   while turtle.getFuelLevel() == 0 do
-       turtle.select(fuel_slot)
-       turtle.refuel(1)
-   end
+  while turtle.getFuelLevel() == 0 do
+    turtle.select(fuel_slot)
+    turtle.refuel(1)
+  end
 end
 
 function resupply(slot,count)
-    if turtle.getItemCount(slot) < count then
-        for check_slot = 1,16 do
-            turtle.select(check_slot)
-            if check_slot ~= build_slot and
-                check_slot ~= window_slot
-            then
-                if turtle.compareTo(slot)
-                then
-                    turtle.transferTo(slot)
-                end
-            end
+  if turtle.getItemCount(slot) < count then
+    for check_slot = 1,16 do
+      turtle.select(check_slot)
+      if check_slot ~= build_slot and
+        check_slot ~= window_slot
+      then
+        if turtle.compareTo(slot)
+        then
+          turtle.transferTo(slot)
         end
+      end
     end
-    print(turtle.getItemCount(slot).." "..count)
-    return turtle.getItemCount(slot) >= count
+  end
+  print(turtle.getItemCount(slot).." "..count)
+  return turtle.getItemCount(slot) >= count
 end
 
 function goUp()
-    while not turtle.up() do
-	turtle.select(junk_slot)
-        turtle.digUp()
-        checkRefuel()
-        sleep(1)
-    end
+  while not turtle.up() do
+    turtle.select(junk_slot)
+    turtle.digUp()
+    checkRefuel()
+    sleep(1)
+  end
 end
 
 function goDown()
-    while not turtle.down() do
-	turtle.select(junk_slot)
-        turtle.digDown()
-        checkRefuel()
-        sleep(1)
-    end
+  while not turtle.down() do
+    turtle.select(junk_slot)
+    turtle.digDown()
+    checkRefuel()
+    sleep(1)
+  end
 end
 
 function digUp()
-	turtle.select(junk_slot)
-	turtle.digUp()
+  turtle.select(junk_slot)
+  turtle.digUp()
 end
 
 function digDown()
-	turtle.select(junk_slot)
-	turtle.digDown()
+  turtle.select(junk_slot)
+  turtle.digDown()
 end
 
 function goBack()
-    if not turtle.back() then
-        turtle.turnRight()
-        turtle.turnRight()
-        goForward()
-        turtle.turnRight()
-        turtle.turnRight()
-    end
+  if not turtle.back() then
+    turtle.turnRight()
+    turtle.turnRight()
+    goForward()
+    turtle.turnRight()
+    turtle.turnRight()
+  end
 end
 
 function goForward()
-    while not turtle.forward() do
-	turtle.select(junk_slot)
-        turtle.dig()
-        checkRefuel()
-        sleep(1)
-    end
+  while not turtle.forward() do
+    turtle.select(junk_slot)
+    turtle.dig()
+    checkRefuel()
+    sleep(1)
+  end
 end
 
 function place(slot)
-    turtle.select(slot)
-    turtle.place()
+  turtle.select(slot)
+  turtle.place()
 end
 
 function placeDown(slot)
-        turtle.select(slot)
-        turtle.placeDown()
+  turtle.select(slot)
+  turtle.placeDown()
 end
 function placeUp(slot)
-        turtle.select(slot)
-        turtle.placeUp()
+  turtle.select(slot)
+  turtle.placeUp()
 end
 
 function doPlaceWall(slot)
-	turtle.select(slot)
-	while not turtle.compare() and not turtle.place() do
-		turtle.select(junk_slot)
-		turtle.dig()
-		turtle.select(slot)
-	end
+  turtle.select(slot)
+  while not turtle.compare() and not turtle.place() do
+    turtle.select(junk_slot)
+    turtle.dig()
+    turtle.select(slot)
+  end
 end
 
 function buildone()
-    turtle.turnLeft()
-    -- grab the bottom center block
-    digUp()
-    placeDown(build_slot)
-    goForward()
-    placeDown(build_slot)
-    goForward()
-    placeDown(build_slot)
-    goBack()
-    doPlaceWall(build_slot)
-    goUp()
-    doPlaceWall(window_slot)
-    goUp()
-    doPlaceWall(build_slot)
-    goUp()
-    doPlaceWall(build_slot)
-    goBack()
-    doPlaceWall(build_slot)
-    -- grab the top center block
-    digDown()
-    goBack()
-    doPlaceWall(window_slot)
-    turtle.turnRight()
-    turtle.turnRight()
-    doPlaceWall(build_slot)
-    goDown()
-    placeUp(build_slot)
-    doPlaceWall(build_slot)
-    goDown()
-    doPlaceWall(window_slot)
-    goDown()
-    placeDown(build_slot)
-    goForward()
-    placeDown(build_slot)
-    goBack()
-    doPlaceWall(build_slot)
-    goBack()
-    placeDown(build_slot)
-    turtle.turnLeft()
+  turtle.turnLeft()
+  -- grab the bottom center block
+  digUp()
+  placeDown(build_slot)
+  goForward()
+  placeDown(build_slot)
+  goForward()
+  placeDown(build_slot)
+  goBack()
+  doPlaceWall(build_slot)
+  goUp()
+  doPlaceWall(window_slot)
+  goUp()
+  doPlaceWall(build_slot)
+  goUp()
+  doPlaceWall(build_slot)
+  goBack()
+  doPlaceWall(build_slot)
+  -- grab the top center block
+  digDown()
+  goBack()
+  doPlaceWall(window_slot)
+  turtle.turnRight()
+  turtle.turnRight()
+  doPlaceWall(build_slot)
+  goDown()
+  placeUp(build_slot)
+  doPlaceWall(build_slot)
+  goDown()
+  doPlaceWall(window_slot)
+  goDown()
+  placeDown(build_slot)
+  goForward()
+  placeDown(build_slot)
+  goBack()
+  doPlaceWall(build_slot)
+  goBack()
+  placeDown(build_slot)
+  turtle.turnLeft()
 end
  
  
@@ -156,14 +156,16 @@ print("Window stuff in slot "..window_slot)
 print("Fuel in slot "..fuel_slot)
 
 for len = 1,tunnel_len do
-    while not resupply(build_slot,14) do
-        print("Need Building materials in slot ".. build_slot)
-        sleep(5)
-    end
-    while not resupply(window_slot,4) do
-        print("Need Window material in slot "..window_slot)
-        sleep(5)
-    end
-    goForward()
-    buildone()
+  while not resupply(build_slot,14) do
+    print("Need Building materials in slot ".. build_slot)
+    sleep(5)
+  end
+  while not resupply(window_slot,4) do
+    print("Need Window material in slot "..window_slot)
+    sleep(5)
+  end
+  goForward()
+  buildone()
 end
+
+-- vim: set filetype=lua et sts=2 sw=2 ts=2 sr sta
